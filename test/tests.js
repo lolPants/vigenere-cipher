5382b471a979b63194bbeab305a416f2587eec2d
Jack Baron <jackmbaron@gmail.com>
2018-06-04 11:37:28 +0100

    some more tests

--- test/tests.js
+++ test/tests.js
@@ -8,4 +8,22 @@ describe('encode()', () => {
   it('should be a function', () => {
     expect(encode).to.be.a('function')
   })
+
+  describe('data types', () => {
+    it('should return a string', () => {
+      expect(encode('test', 'haha')).to.be.a('string')
+    })
+  })
+})
+
+describe('decode()', () => {
+  it('should be a function', () => {
+    expect(decode).to.be.a('function')
+  })
+
+  describe('data types', () => {
+    it('should return a string', () => {
+      expect(decode('test', 'haha')).to.be.a('string')
+    })
+  })
 })
