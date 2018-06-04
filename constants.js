4007a3381d34aee67fc0b035dc9768bf7b181b0a
Jack Baron <jackmbaron@gmail.com>
2018-06-04 10:52:23 +0100

    Add symbols

--- constants.js
+++ constants.js
@@ -1,4 +1,4 @@
 const AtoZ = 'abcdefghijklmnopqrstuvwxyz'
-const DEFAULT_ALPHABET = `${AtoZ.toUpperCase()}${AtoZ.toLowerCase()}01234567890`
+const DEFAULT_ALPHABET = `${AtoZ.toUpperCase()}${AtoZ.toLowerCase()}01234567890 ._-+=/\\;:'"!@#$%^&*()`
 
 module.exports = { DEFAULT_ALPHABET }
