f56ba613df7712844f03a1230c04108fd0638b39
Jack Baron <jackmbaron@gmail.com>
2018-06-04 10:56:16 +0100

    Encoding function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const { DEFAULT_ALPHABET } = require('./constants')
 
-const generateSet = (alphabet = DEFAULT_ALPHABET) => Array.from(new Array(alphabet.length)).map((x, i) => alphabet[i])
-const generateMap = (alphabet = DEFAULT_ALPHABET) => Array.from(new Array(alphabet.length)).map(() => generateSet(alphabet))
+const generateSet = (alphabet = DEFAULT_ALPHABET, offset = 0) => Array.from(new Array(alphabet.length)).map((x, i) => alphabet[(i + offset) % alphabet.length])
+const generateMap = (alphabet = DEFAULT_ALPHABET) => Array.from(new Array(alphabet.length)).map((x, i) => generateSet(alphabet, i))
 
-console.log(generateMap('abcd'))
+/**
+ * Encode using a Vigenère cipher
+ * @param {string} plaintext Plaintext to encode
+ * @param {string} key Cryptographic Cipher Key
+ * @param {string|string[]} [alphabet] Lexical Alphabet
+ * @returns {string}
+ */
+const encode = (plaintext, key, alphabet = DEFAULT_ALPHABET) => {
+  let map = generateMap(alphabet)
+  return plaintext.split('').reduce((a, c, i) => `${a}${map[alphabet.indexOf(key[i % key.length])][alphabet.indexOf(c)]}`, '')
+}
+
+console.log(encode('i fucking love memes', 'haha content'))
