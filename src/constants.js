f4daa61b0216e804add0a01196f32f943556f70c
Jack Baron <jackmbaron@gmail.com>
2018-06-04 11:15:50 +0100

    Linting

--- src/constants.js
+++ src/constants.js
@@ -8,12 +8,13 @@ const DEFAULT_ALPHABET = `${AtoZ.toUpperCase()}${AtoZ.toLowerCase()}01234567890 
  * @param {number} [offset] Offset to shift the row by
  * @returns {string[]}
  */
-const generateRow = (alphabet = DEFAULT_ALPHABET, offset = 0) => Array.from(new Array(alphabet.length)).map((x, i) => alphabet[(i + offset) % alphabet.length])
+const generateRow = (alphabet = DEFAULT_ALPHABET, offset = 0) => Array.from(new Array(alphabet.length))
+  .map((x, i) => alphabet[(i + offset) % alphabet.length])
 
 /**
  * Generate a full Vigenère square
- * @param {string|string[]} alphabet Lexical Alphabet to generate using
- * @returns {string[][]}
+ * @param {string} alphabet Lexical Alphabet to generate using
+ * @returns {(string[])[]}
  */
 const generateSquare = (alphabet = DEFAULT_ALPHABET) => Array.from(new Array(alphabet.length)).map((x, i) => generateRow(alphabet, i))
 
